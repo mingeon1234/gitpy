ff982cec087e6596164e55122f5d77b9ea8ee8e1
mingeon1234 <minkun0714@gmail.com>
2022-10-27 23:28:17 +0900

    mingeon

--- A.py
+++ A.py
@@ -1,4 +1,11 @@
-print("안니영")
-print(10+10)
-print(10*2)
-print(10.2+10.4)
+arr=[1,2,3,4,"안니영",True]
+num = 1
+
+while num == 2 :
+    print(1)
+else:
+    print(3)
+
+
+for i in range(1,10,1):
+    print(i)
